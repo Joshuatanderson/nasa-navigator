94531e185d103ba29f2ec1d9bb567b03c3229ea4
Joshua Anderson <joshuatanderson1@gmail.com>
2020-12-06 17:10:49 -0600

    feat: theme change works... haptics, maybe

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Menu from './components/Menu';
 import Page from './pages/Page';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { IonApp, IonRouterOutlet, IonSplitPane } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { Redirect, Route } from 'react-router-dom';
+import { Plugins } from "@capacitor/core";
+
+import ThemeContext from "./contexts/Theme"
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -22,26 +25,72 @@ import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
 /* Theme variables */
-import './theme/variables.css';
+import './theme/variables.scss';
 import DailyPicture from './pages/DailyPicture';
 import SearchPictures from './pages/SearchPictures';
 import NaturalEvents from './pages/NaturalEvents';
 
 const App: React.FC = () => {
+  const [isDark, setIsDark] = useState(true);
+	const [isInit, setIsInit] = useState(false);
+
+  // TODO: check if user prefers dark mode
+	useEffect(() => {
+		if (isInit) {
+			// set css
+			document.body.classList.toggle("dark", isDark);
+			storeTheme(isDark);
+		}
+	}, [isDark, isInit]);
+
+	useEffect(() => {
+		setUserPreferences();
+		async function setUserPreferences() {
+			const stored = await getStoredTheme();
+			if (stored !== null) {
+				setIsDark(stored === "true" ? true : false);
+				console.log(`isDark: ${stored === "true" ? true : false}`);
+			}
+		}
+		setIsInit(true);
+	}, []);
+
+	const toggleTheme = () => {
+		setIsDark(!isDark);
+	};
+
+	/**
+	 * Stores if the device prefers dark mode
+	 * @param isDark - theme boolean
+	 */
+	async function storeTheme(isDark: boolean): Promise<void> {
+		await Plugins.Storage.set({
+			key: "isDark",
+			value: `${isDark ? "true" : "false"}`,
+		});
+	}
+
+	async function getStoredTheme() {
+		const stored = await Plugins.Storage.get({ key: "isDark" });
+		return stored.value;
+	}
+
   return (
-    <IonApp>
-      <IonReactRouter>
-        <IonSplitPane contentId="main">
-          <Menu />
-          <IonRouterOutlet id="main">
-            <Route path="/daily-pic" component={DailyPicture} exact />
-            <Route path="/natural-events" component={NaturalEvents} exact />
-            <Route path="/search-pictures" component={SearchPictures} exact />
-            <Redirect from="/" to="/daily-pic" exact />
-          </IonRouterOutlet>
-        </IonSplitPane>
-      </IonReactRouter>
-    </IonApp>
+    <ThemeContext.Provider value={isDark}>
+      <IonApp>
+        <IonReactRouter>
+          <IonSplitPane contentId="main">
+            <Menu toggleTheme={toggleTheme}/>
+            <IonRouterOutlet id="main">
+              <Route path="/daily-pic" component={DailyPicture} exact />
+              <Route path="/natural-events" component={NaturalEvents} exact />
+              <Route path="/search-pictures" component={SearchPictures} exact />
+              <Redirect from="/" to="/daily-pic" exact />
+            </IonRouterOutlet>
+          </IonSplitPane>
+        </IonReactRouter>
+      </IonApp>
+    </ThemeContext.Provider>
   );
 };
 
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -10,10 +10,11 @@ import {
   IonNote,
   IonToggle,
 } from '@ionic/react';
-
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import { archiveOutline, archiveSharp, bookmarkOutline, calendarOutline, calendarSharp, heartOutline, heartSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, searchOutline, searchSharp, thunderstormOutline, thunderstormSharp, trashOutline, trashSharp, warningOutline, warningSharp } from 'ionicons/icons';
+import {HapticsImpactStyle, Plugins} from "@capacitor/core";
+
 import './Menu.css';
 
 interface AppPage {
@@ -44,8 +45,17 @@ const appPages: AppPage[] = [
   },
 ];
 
-const Menu: React.FC = () => {
+interface MenuProps {
+  toggleTheme: () => void;
+}
+
+const Menu = ({toggleTheme}: MenuProps) => {
   const location = useLocation();
+
+  const handleToggleTheme = () => {
+    Plugins.Haptics.impact({style: HapticsImpactStyle.Medium});
+    toggleTheme();
+  }
 
   return (
     <IonMenu contentId="main" type="overlay">
@@ -64,8 +74,8 @@ const Menu: React.FC = () => {
             );
           })}
           <IonItem>
-            <IonToggle></IonToggle> 
-            <IonLabel>Dark Mode</IonLabel>
+            <IonToggle onIonChange={handleToggleTheme}></IonToggle> 
+            <IonLabel>Toggle Theme</IonLabel>
           </IonItem>
         </IonList>
       </IonContent>
